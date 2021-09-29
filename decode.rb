4387208a3a5b10c8ad4230f96045dc4d9d722cb9
Kevin <kevinsevinche@gmail.com>
2021-09-29 14:50:20 -0500

    Update README.md and algorithms

--- decode.rb
+++ decode.rb
@@ -1,25 +1,39 @@
-def decode_char(string)
-  string2 = "#{string} "
-  code_index = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
-                 '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
-                 '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
-                 '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z' }
-  arrays = { 'big' => [], 'small' => [] }
-  spaces = { 'space_detect' => false, 'over_space_detect' => true }
-  string2.each_char do |e|
-    if ['.', '-'].include?(e)
-      spaces = { 'space_detect' => false, 'over_space_detect' => true }
-      arrays['small'].push(e)
-    elsif spaces['space_detect'] == false
-      arrays['big'].push(code_index[arrays['small'].join])
-      arrays['small'] = []
-      spaces['space_detect'] = true
-    elsif spaces['over_space_detect']
-      arrays['big'].push(' ')
-      spaces['over_space_detect'] = false
-    end
+@morse = [
+  '.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....',
+  '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.',
+  '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-',
+  '-.--', '--..'
+]
+
+@abc = 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+       'W', 'X', 'Y', 'Z'
+
+def decode_word(word)
+  decoded_word = ''
+  word_splitted = word.split
+  word_splitted.each do |w|
+    decoded_word += decode_char(w)
   end
-  print arrays['big'].join
+  decoded_word
 end
 
-decode_char('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...')
+def decode_char(char)
+  @morse.each_with_index do |m, idx|
+    return (@abc[idx]).to_s if m == char
+  end
+end
+
+def decode(message)
+  chars = message.chars
+  chars.each_with_index do |m, idx|
+    message[idx] = ' ' if m != '.' && m != '-' && m != ' '
+  end
+  decoded_message = ''
+  message_splitted = message.split('   ')
+  message_splitted.each do |m|
+    decoded_message = "#{decoded_message} #{decode_word(m)}"
+  end
+  decoded_message
+end
+
+print decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...')
