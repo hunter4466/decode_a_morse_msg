470ce64cd56fa6d3001cb236b80ae8536f625afd
Kevin <kevinsevinche@gmail.com>
2021-09-29 14:35:26 -0500

    Add algorith to decode morse message

--- decode.rb
+++ decode.rb
@@ -5,6 +5,21 @@ def decode_char(string)
                  '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
                  '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z' }
   arrays = { 'big' => [], 'small' => [] }
+  spaces = { 'space_detect' => false, 'over_space_detect' => true }
+  string2.each_char do |e|
+    if ['.', '-'].include?(e)
+      spaces = { 'space_detect' => false, 'over_space_detect' => true }
+      arrays['small'].push(e)
+    elsif spaces['space_detect'] == false
+      arrays['big'].push(code_index[arrays['small'].join])
+      arrays['small'] = []
+      spaces['space_detect'] = true
+    elsif spaces['over_space_detect']
+      arrays['big'].push(' ')
+      spaces['over_space_detect'] = false
+    end
+  end
+  print arrays['big'].join
 end
 
 decode_char('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...')
